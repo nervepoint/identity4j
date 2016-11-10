dc66b612d115bed6418b00509f4e5b671cc9d9ec
Lee David Painter <lee@ludup.com>
2016-11-10 23:20:58 +0000

    New options in SshKeyVerifier

--- identity4j-script-ssh/src/main/java/com/identity4j/connector/script/ssh/SshKeyVerifier.java
+++ identity4j-script-ssh/src/main/java/com/identity4j/connector/script/ssh/SshKeyVerifier.java
@@ -1,7 +1,12 @@
 package com.identity4j.connector.script.ssh;
 
+import java.util.Set;
+
 public interface SshKeyVerifier {
 
 	boolean verifyKey(String host, int port, String algorithm, int bitLength, byte[] encoded, String fingerprint, byte[] formatted);
 
+	Set<String> getKeyAlgorithms();
+
+	int getCount();
 }
