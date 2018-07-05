3922e91c9f86b5e5d2ffdcf2ddd043ea90a0277b
ludup <lee@ludup.com>
2018-07-05 12:13:17 +0100

    Change to commons lang3

--- identity4j-active-directory-jndi/src/main/java/com/identity4j/connector/jndi/activedirectory/ActiveDirectoryConnector.java
+++ identity4j-active-directory-jndi/src/main/java/com/identity4j/connector/jndi/activedirectory/ActiveDirectoryConnector.java
@@ -56,8 +56,8 @@ import javax.naming.ldap.LdapName;
 import javax.naming.ldap.Rdn;
 
 import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.lang.ArrayUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
