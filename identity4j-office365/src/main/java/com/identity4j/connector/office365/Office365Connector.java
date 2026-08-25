0b1a69846cd78850c6fc3986267a294ff87c7f7c
brett-smith <brett@nervepoint.com>
2026-08-25 19:42:40 +0100

    Build fixes: Add missing Collections import to Office365Connector

--- identity4j-office365/src/main/java/com/identity4j/connector/office365/Office365Connector.java
+++ identity4j-office365/src/main/java/com/identity4j/connector/office365/Office365Connector.java
@@ -26,6 +26,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
